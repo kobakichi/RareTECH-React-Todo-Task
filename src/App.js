0dc88d35df20352c53b7986e67648cb395a62dda
kobakichi <fenderstrat57vin@gmail.com>
2021-12-06 05:26:59 +0900

    Todoリストの完成形

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Input from './components/Input';
+import Title from './components/Title';
+import Todolist from './components/Todolist';
+import Search from './components/Search';
+
 
 function App() {
+// 最初のTodoリストの配列
+const initialState = [
+  {
+    task: 'Task1'
+  },
+  {
+    task: 'Task2'
+  },
+]
+
+// 上で定義した配列をuseStateでtodosに入れる
+const [todos, setTodos] = useState(initialState);
+
+// Searchコンポーネントで入力されたキーワードを保存するステート
+const [searchKeyword, setSearchKeyword] = useState('');
+
+const handleSearch = (event) => {
+  setSearchKeyword(event.target.value);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Title title={'Todo List'} />
+      <Input todos={todos} setTodos={setTodos} />
+      <Search todos={todos} value={searchKeyword} onChange={handleSearch}/>
+      <Todolist todos={todos} setTodos={setTodos} searchKeyword={searchKeyword} setSearchKeyword={setSearchKeyword}/>
     </div>
   );
-}
+};
 
 export default App;
