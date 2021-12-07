d8fccb26dc103f11df475a833a403df8edb744a4
kobakichi <fenderstrat57vin@gmail.com>
2021-12-08 04:53:28 +0900

    prettierを使ってインデントを揃えた

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Todolist = (props) => {
-
-  const handleRemoveTask = index => {
-    const newTodos = ( todos => [...todos].filter((todo,todoIndex) => todoIndex !== index));
-    props.setTodos(newTodos)
-  }
+  const handleRemoveTask = (index) => {
+    const newTodos = (todos) =>
+      [...todos].filter((todo, todoIndex) => todoIndex !== index);
+    props.setTodos(newTodos);
+  };
 
   const handleOnEdit = (index, value) => {
     const newTodos = props.todos.map((todo, todoindex) => {
@@ -15,26 +15,41 @@ const Todolist = (props) => {
       return todo;
     });
     props.setTodos(newTodos);
-  }
+  };
 
   return (
     <div className="task-area">
       <ul className="todolist">
-        { props.todos.filter((val) => {
-          if (props.searchKeyword === '') {
-            return val;
-          } else if (
-            val.task.toString().toLowerCase().includes(props.searchKeyword.toString().toLowerCase())
-          ) {
-            return val;
-          } return false;
-        }).map((todo, index) => (
-          <li className="todo" key={ index }><input type="text" className="editForm" value={todo.task} onChange={(event) => handleOnEdit(index, event.target.value)} />
-          <span onClick={() => handleRemoveTask(index) }><i className="far fa-trash-alt"></i></span></li>
-        ))}
+        {props.todos
+          .filter((val) => {
+            if (props.searchKeyword === "") {
+              return val;
+            } else if (
+              val.task
+                .toString()
+                .toLowerCase()
+                .includes(props.searchKeyword.toString().toLowerCase())
+            ) {
+              return val;
+            }
+            return false;
+          })
+          .map((todo, index) => (
+            <li className="todo" key={index}>
+              <input
+                type="text"
+                className="editForm"
+                value={todo.task}
+                onChange={(event) => handleOnEdit(index, event.target.value)}
+              />
+              <span onClick={() => handleRemoveTask(index)}>
+                <i className="far fa-trash-alt"></i>
+              </span>
+            </li>
+          ))}
       </ul>
     </div>
   );
 };
 
-export default Todolist
+export default Todolist;
