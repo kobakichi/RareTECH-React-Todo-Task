99a06066382c899b072430f97d3ea770b87ccb6f
kobakichi <fenderstrat57vin@gmail.com>
2021-12-15 04:57:51 +0900

    カスタムフック化のため、ロジックをuseApp.jsへ分離させた

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@
  * @package src
  */
 
-import React, { useState } from "react";
+import React from "react";
 /** components */
 import { AddTodo } from "./components/AddTodo";
 import { Todolist } from "./components/Todolist";
 import { SearchTodo } from "./components/SearchTodo";
-/* constants */
-import { INIT_TODO_LIST, INIT_TODO_LIST_COUNT } from "./constants/data";
+/* hooks */
+import { useApp } from "./hooks/useApp";
 /** styles */
 import "./App.css";
 
@@ -18,98 +18,28 @@ import "./App.css";
  * @returns
  */
 export const App = () => {
-  // todoリストの初期値
-  const [todos, setTodos] = useState(INIT_TODO_LIST);
-  // タスク追加用の入力値
-  const [addInputValue, setAddInputValue] = useState("");
-  //todo 採番ID
-  const [uniqueId, setUniqueId] = useState(INIT_TODO_LIST_COUNT);
-  // Searchコンポーネントで入力されたキーワードを保存するステート
-  const [searchKeyword, setSearchKeyword] = useState("");
-
-  /**
-   *タスクが入力されたらデータを保持する処理、更新処理
-   * @param {*} event
-   */
-  const handleChangeAddInputTodo = (event) => {
-    setAddInputValue(event.target.value);
-  };
-
-  /**
-   * タスク追加処理
-   * @param {*} event
-   */
-  const handleAddTodo = (event) => {
-    // エンターキーが押された時にTodoを追加する
-    if (event.key === "Enter" && addInputValue !== "") {
-      const nextUniqueId = uniqueId + 1;
-      setTodos([
-        ...todos,
-        {
-          id: nextUniqueId,
-          title: addInputValue,
-        },
-      ]);
-      //採番IDを更新
-      setUniqueId(nextUniqueId);
-      //todo追加後、入力値をリセットする
-      setAddInputValue("");
-    }
-  };
-  /**
-   * タスク削除処理
-   * @param {*} index
-   * 削除する時にconfirmを出している
-   */
-  const handleRemoveTodo = (targetId, targetTitle) => {
-    if (window.confirm(` 「${targetTitle}」のtodoを削除しますか？`)) {
-      // 削除するid以外のtodoリストを再編集
-      const newTodos = todos.filter((todo) => todo.id !== targetId);
-      setTodos(newTodos);
-    }
-  };
-
-  /**
-   * タスク編集処理
-   * @param {*} index
-   * @param {*} value
-   */
-  const handleOnEdit = (index, value) => {
-    const newTodos = todos.map((todo, todoIndex) => {
-      if (todoIndex === index) {
-        todo.title = value;
-      }
-      return todo;
-    });
-    setTodos(newTodos);
-  };
-
-  /**
-   * 検索キーワード更新処理 キーワードを保持する
-   * @param {*} event
-   */
-  const handleChangeSearchKeyword = (event) => {
-    setSearchKeyword(event.target.value);
-  };
+  /* hooks */
+  const [states, actions] = useApp();
 
   return (
     <div className="wrapper">
       <h1>Todo List</h1>
       <AddTodo
-        addInputValue={addInputValue}
-        handleChangeAddInputTodo={handleChangeAddInputTodo}
-        handleAddTodo={handleAddTodo}
+        addInputValue={states.addInputValue}
+        handleChangeAddInputTodo={actions.handleChangeAddInputTodo}
+        handleAddTodo={actions.handleAddTodo}
       />
       <SearchTodo
-        todos={todos}
-        value={searchKeyword}
-        handleChangeSearchKeyword={handleChangeSearchKeyword}
+        todos={states.todos}
+        value={states.searchKeyword}
+        handleChangeSearchKeyword={actions.handleChangeSearchKeyword}
       />
       <Todolist
-        todos={todos}
-        searchKeyword={searchKeyword}
-        handleRemoveTodo={handleRemoveTodo}
-        handleOnEdit={handleOnEdit}
+        todos={states.todos}
+        searchKeyword={states.searchKeyword}
+        filteredList={actions.filteredList}
+        handleRemoveTodo={actions.handleRemoveTodo}
+        handleOnEdit={actions.handleOnEdit}
       />
     </div>
   );
