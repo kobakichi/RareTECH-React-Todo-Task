566c9ae7f9fc2e896aeaf916171860fb97dd34c6
yuki <yuki.onishi1129@gmail.com>
2022-01-29 02:02:18 +0900

    Todo編集処理修正

--- src/components/TodoDetail.jsx
+++ src/components/TodoDetail.jsx
@@ -12,15 +12,68 @@ import React from "react";
 export const TodoDetail = (props) => {
   // props
   const { todo, handleOnEdit, handleRemoveTodo } = props;
+  /* local */
+  const [editFlg, setEditFlg] = React.useState(false);
+  const [editInputValue, setEditInputValue] = React.useState(todo.title);
+
+  /**
+   * 編集モードON
+   */
+  const onChangeEditMode = () => {
+    setEditFlg(true);
+  };
+
+  /**
+   * 編集用input value変更処理
+   * @param {*} editTitle
+   */
+  const onChangeEditTodo = (editTitle) => {
+    setEditInputValue(editTitle);
+  };
+
+  /**
+   * タスク編集処理 (Enterをクリックした際に発火)
+   * @param {*} event
+   */
+  const onDecideEditTodo = (event) => {
+    if (event.key === "Enter") {
+      handleOnEdit(todo.id, editInputValue);
+      setEditFlg(false);
+    }
+  };
+
+  /**
+   * タスク編集処理 (inputからフォーカスアウトした際に発火)
+   */
+  const onBlurDecideEditTodo = () => {
+    handleOnEdit(todo.id, editInputValue);
+    setEditFlg(false);
+  };
 
   return (
     <li className="todo" key={todo.id}>
-      <input
-        type="text"
-        className="editForm"
-        value={todo.title}
-        onChange={(event) => handleOnEdit(todo.id, event.target.value)}
-      />
+      {editFlg ? (
+        <div>
+          <input
+            type="text"
+            className="editForm"
+            value={editInputValue}
+            onChange={(event) => onChangeEditTodo(event.target.value)}
+            onKeyPress={onDecideEditTodo}
+            onBlur={onBlurDecideEditTodo}
+          />
+        </div>
+      ) : (
+        <div onClick={onChangeEditMode}>
+          <input
+            disabled
+            type="text"
+            className="detailForm"
+            value={todo.title}
+          />
+        </div>
+      )}
+
       <span onClick={() => handleRemoveTodo(todo.id, todo.title)}>
         <i className="far fa-trash-alt"></i>
       </span>
